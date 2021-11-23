699c78ec6cfd0f1be4250deb716444d0806781ff
MyeongHoonNam <codenmh0822@naver.com>
2021-11-23 18:46:30 +0900

    변수 기본 타입 : 튜플, 객체, 진위값

--- note/1_type-basic.ts
+++ note/1_type-basic.ts
@@ -9,3 +9,23 @@ const arr: Array<number> = [1, 2, 3]
 const itemsNumber: number[] = [1, 2, 3]
 const heroes: Array<string> = ['Hoon', 'Myeong']
 const itemsString: string[] = ['Hoon', 'Myeong']
+
+// 튜플: 배열의 인덱스에 까지 타입이 정해져 있는 자료구조
+const address: [string, number] = ['Hoon', 26]
+
+// 객체
+const obj: object = {}
+
+// const person: object = {
+//   name: 'Hoon',
+//   age: 26
+// }
+
+// 위의 방법과 다른 좀 더 구체적인 명세가 가능
+const person: {name: string, age: number} = {
+  name: 'Hoon',
+  age: 26
+}
+
+// 진위값
+const show: boolean = true
