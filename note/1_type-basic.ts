c56ca294664e9380a409b2c0c7c7d95288fafc6e
MyeongHoonNam <codenmh0822@naver.com>
2021-11-23 18:38:51 +0900

    변수 기본 타입 : 문자열, 숫자, 배열

---
+++ note/1_type-basic.ts
@@ -0,0 +1,11 @@
+// 문자열 선언
+const str: string = 'hello'
+
+// 숫자 선언
+const num: number = 10
+
+// 배열 선언
+const arr: Array<number> = [1, 2, 3]
+const itemsNumber: number[] = [1, 2, 3]
+const heroes: Array<string> = ['Hoon', 'Myeong']
+const itemsString: string[] = ['Hoon', 'Myeong']
