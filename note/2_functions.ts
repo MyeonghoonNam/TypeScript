f491a06c9f5cd4cc8cb3d2c39237d6d5a8f98be9
MyeongHoonNam <codenmh0822@naver.com>
2021-11-23 23:12:53 +0900

    함수 타입 : 파라미터를 제한하는 특성

--- note/2_functions.ts
+++ note/2_functions.ts
@@ -1,9 +1,8 @@
 // 함수의 파라미터에 타입을 정의하는 방식
-const sum = (a: number, b: number) => {
-  return a + b
-}
+// const sum = (a: number, b: number) => {
+//   return a + b
+// }
 
-sum(10, 20)
 
 // 함수의 반환 값에 타입을 정의하는 방식
 const add = (): number => {
@@ -14,3 +13,6 @@ const add = (): number => {
 const plus = (a: number, b: number): number => {
   return a + b
 }
+
+// js와 달리 파라미터의 수의 제한이 존재한다.
+// plus(10, 20, 30, 40) -> 오류 발생
