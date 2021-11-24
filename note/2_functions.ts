699cdcbbcda1bba7d61bc701bd2ff7f8fa18d990
MyeongHoonNam <codenmh0822@naver.com>
2021-11-24 13:35:50 +0900

    함수 타입: 옵셔널 파라미터

--- note/2_functions.ts
+++ note/2_functions.ts
@@ -16,3 +16,12 @@ const plus = (a: number, b: number): number => {
 
 // js와 달리 파라미터의 수의 제한이 존재한다.
 // plus(10, 20, 30, 40) -> 오류 발생
+
+// 함수의 옵셔널 파라미터
+// ?: 기호 값이 있어도 되고 없어도 된다.
+const log = (a: number, b?: number): number => {
+  return a + b
+}
+
+log(10)
+log(10, 20)
