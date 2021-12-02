8d3bfde45d231a53fd7a49817e4f0f91f5600696
MyeongHoonNam <codenmh0822@naver.com>
2021-12-02 23:30:39 +0900

    인터페이스를 활용한 함수 인자 정의

--- note/3_interface.ts
+++ note/3_interface.ts
@@ -4,9 +4,21 @@ interface User {
   name: string;
 }
 
+// 변수에 인터페이스 활용
 const hoon: User = {
   age: 33,
   name: '훈'
 }
 
-console.log(hoon.age)
+// 함수에 인터페이스 활용
+function getUser(user: User): void {
+  console.log(user)
+}
+
+const myeong = {
+  name: '명'
+}
+
+// 인자가 오류 발생 인터페이스 형태와 다르다.
+// getUser(myeong)
+getUser(hoon)
