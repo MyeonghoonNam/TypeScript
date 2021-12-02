6d908e7174fb0c6ac221c3c0c84d670f3aa4f893
MyeongHoonNam <codenmh0822@naver.com>
2021-12-03 03:26:29 +0900

    인터페이스를 통한 함수 구조 정의

--- note/3_interface.ts
+++ note/3_interface.ts
@@ -22,3 +22,14 @@ const myeong = {
 // 인자가 오류 발생 인터페이스 형태와 다르다.
 // getUser(myeong)
 getUser(hoon)
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+  // 인자 지정
+  (a: number, b: number): number
+}
+
+// 인터페이스 인자와 함수 인자 네이밍이 동일할 필요는 없다.
+const sum:SumFunction = (a2: number, b2: number): number => {
+  return a2 + b2
+}
