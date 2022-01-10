f0fcb2302c8b582f3bcaa291e0776863c82ee371
MyeongHoonNam <codenmh0822@naver.com>
2022-01-10 21:00:52 +0900

    인터페이스와 제네릭을 이용한 타입 추론

--- note/9_type-inference.ts
+++ note/9_type-inference.ts
@@ -1,9 +1,21 @@
 // 타입추론
 // 타입스크립트가 코드를 해석해 나가는 동작을 의미
+// 변수를 선언하거나 초기화 할 때 타입이 추론된다.
 
 // number + string => string의 기본적인 타입 추론 
 const getA = (number = 10) => {
   const str = '10';
 
   return number + str;
+}
+
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+// 인터페이스와 제네릭을 이용한 타입추론
+const shoppingItem: Dropdown<string> = {
+  value: 'nike',
+  title: 'shoes',
 }
