3682e93d940de21edfa4d1c4c210726496dbcbcd
MyeongHoonNam <codenmh0822@naver.com>
2022-01-11 23:29:45 +0900

    Best Common Type 추론 방식

--- note/9_type-inference.ts
+++ note/9_type-inference.ts
@@ -33,3 +33,15 @@ const detailedItem: DetailedDropdown<string> = {
   value: '2022-01-11',
   tag: 'm'
 }
+
+// Best Common Type 추론 방식
+// 타입들을 union 방식으로 묶어 가장 가까운 타입을 지정해 나가는 추론 과정을 말한다.
+const arr = [1, 2, true, 'string'];
+
+window.onmousedown = function(mouseEvent) {
+  console.log(mouseEvent.button);   //<- OK
+  console.log(mouseEvent.kangaroo); //<- Error!
+};
+
+// 위 코드를 타입스크립트 검사기 관점에서 보면 window.onmousedown에 할당되는 함수의 타입을 추론하기 위해 window.onmousedown 타입을 검사한다.
+// 타입 검사가 끝나고 나면 함수의 타입이 마우스 이벤트와 연관이 있다고 추론하기 때문에 mouseEvent 인자에 button 속성은 있지만 kangaroo 속성은 없다고 결론을 내리게 된다.
