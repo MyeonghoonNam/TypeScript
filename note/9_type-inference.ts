d23490b0cb494ed263e1ea9dd2f72f5c3be3147a
MyeongHoonNam <codenmh0822@naver.com>
2022-01-11 23:24:42 +0900

    복잡한 구조의 타입추론

--- note/9_type-inference.ts
+++ note/9_type-inference.ts
@@ -14,8 +14,22 @@ interface Dropdown<T> {
   title: string;
 }
 
+interface DetailedDropdown<T> extends Dropdown<T>{
+  description: string;
+  tag: T;
+}
+
 // 인터페이스와 제네릭을 이용한 타입추론
 const shoppingItem: Dropdown<string> = {
   value: 'nike',
   title: 'shoes',
+}
+
+// 복잡한 방식의 타입추론
+// 지정된 타입이 DetailedDropdown -> Dropdown순으로 상위로 지정된다.
+const detailedItem: DetailedDropdown<string> = {
+  title: 'shirt',
+  description: 'adidas',
+  value: '2022-01-11',
+  tag: 'm'
 }
