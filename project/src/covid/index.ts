bd30c6147e3131c78fa9441ca589865ed7793402
MyeongHoonNam <codenmh0822@naver.com>
2022-01-31 00:10:10 +0900

    api 함수 타입 구체화

--- project/src/covid/index.ts
+++ project/src/covid/index.ts
@@ -1,6 +1,28 @@
-export interface CovidSummaryResponse {
-  Countries: [];
+interface Country {
+  Country: string;
+  CountryCode: string;
   Date: string;
-  Global: any;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecoverd: number;
+  Premium: any;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecoverd: number;
+}
+
+interface Global {
+  NewConfiremd: number;
+  NewDeaths: number,
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecoverd: number;
+}
+export interface CovidSummaryResponse {
+  Countries: Country[];
+  Date: string;
+  Global: Global;
   Message: string;
 }
